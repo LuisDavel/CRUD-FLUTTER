16eacd9a51503a763c9c418f4580508ea84a1234
Davel <luisdavel90@outlook.com>
2022-08-23 23:18:12 -0300

    v1

---
+++ android/app/src/main/kotlin/com/example/desafiosdeo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafiosdeo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const productBaseUrl =
+      'https://meuprojeto-85c11-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://meuprojeto-85c11-default-rtdb.firebaseio.com/orders';
+}
